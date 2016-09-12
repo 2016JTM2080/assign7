45cc45b75f48afa385f05136a2d9186948b84c2c
2016JTM2080 <prakhararya25@gmail.com>
2016-09-12 14:47:52 +0530

    program for problem statement 2

--- ps2.py
+++ ps2.py
@@ -1,3 +1,17 @@
 ###### this is the second .py file ###########
 
-####### write your code here ##########
+import random    #importing radom from the python library
+  
+count=0     
+#count denotes the no of users in the unit circle
+
+for u in range(1,101):
+	(X,Y)=(random.random()*2- 1, random.random()*2-1)
+	u=u+1
+	
+	if((X**2+Y**2)<=1):
+		count+=1
+
+perc=(float(count)/100)*100
+
+print "Percentage of users in the unit circle around the MSC is" ,perc

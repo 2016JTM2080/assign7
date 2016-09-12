29a0f7e57d7e9c24f320e3283c1ec2f4fe559f2f
2016JTM2080 <prakhararya25@gmail.com>
2016-09-12 16:45:48 +0530

    Updted program for ps1

--- ps1.py
+++ ps1.py
@@ -1,4 +1,27 @@
 ###### This is the first .py file ###########
+#import sys
+#str1 = sys.argv
+#word_freq = {}
+#from collections import Counter
+#print ("top 3 frequent words:")
+#for i in range(1,len(str1)) :
+#	lis = sorted(Counter(str[i]),key=lambda (i,j):(-j,i))[:3]
+#	for word,freq in lis :
+#		print word, freq
+#		def perms(s):        
+#	    		if(len(s)==1): return [s]
+#	    		result=[]
+#	    		for i,v in enumerate(s):
+#				result += [v+p for p in perms(s[:i]+s[i+1:])]
+#	                return result
+
+
+#	print('\n'.join(perms(word)))
+#
+
+
+
+###### This is the first .py file ###########
 
 str1 = raw_input(" enter the srting : ")
 word_freq = {}
@@ -7,12 +30,13 @@ print ("top 3 frequent words:")
 lis = sorted(Counter(str1.split()).items(),key=lambda (i,j):(-j,i))[:3]
 for word,freq in lis:
 	print word, freq
+for word,freq in lis:
 	def perms(s):        
-    		if(len(s)==1): return [s]
-    		result=[]
-    		for i,v in enumerate(s):
-        		result += [v+p for p in perms(s[:i]+s[i+1:])]
-    		return result
-
+	    if(len(s)==1): 
+		return [s]
+	    result=[]
+	    for i,v in enumerate(s):
+		result += [v+p for p in perms(s[:i]+s[i+1:])]
+	    return result
 
 	print('\n'.join(perms(word)))
